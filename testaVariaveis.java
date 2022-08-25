ac388488f75f795e5f67913c97f9b9443688549f
Gilberto mascena <gilbertomascena@gmail.com>
2022-08-25 16:14:55 -0300

    Adicionando novos arquivos

---
+++ testaVariaveis.java
@@ -0,0 +1,13 @@
+
+public class testaVariaveis {
+	
+	public static void main(String[] args) {
+		System.out.println("ola novo teste");
+		
+		int idade;
+		idade = 42;
+		System.out.println(idade);
+		
+		System.out.println("a minha idade e " + idade + " anos.");
+	}
+}
