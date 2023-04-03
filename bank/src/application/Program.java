5b01060cd4df4c3af779cee993b37c606ca84847
Gilberto Mascena <gilbertomascena@gmail.com>
2023-04-02 21:46:32 -0300

    Add new files

--- bank/src/application/Program.java
+++ bank/src/application/Program.java
@@ -15,7 +15,10 @@ public class Program {
 		
 		Account acc1 = bacc;
 		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
-		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+		Account sacc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+		Account acc6 = new Account(1001, "Gilberto", 1000.0);
+		Account sacc7 =new SavingsAccount(1010, "Silva", 1000.0, 0.01);
+		Account bacc8 = new BusinessAccount(1011, "Alex", 1000.0, 500.0);
 		
 		// DOWNCASTING
 		
@@ -23,17 +26,26 @@ public class Program {
 		acc4.loan(100.0);
 		
 		// BusinessAccount acc5 = (BusinessAccount)acc3;
-		if(acc3 instanceof BusinessAccount) {
-			BusinessAccount acc5 = (BusinessAccount)acc3;
+		if(sacc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)sacc3;
 			acc5.loan(200.0);
 			System.out.println("Laon!");
 		}
 		
-		if(acc3 instanceof SavingsAccount) {
-			SavingsAccount acc5 = (SavingsAccount)acc3;
+		if(sacc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)sacc3;
 			acc5.updateBalance();
 			System.out.println("Update!");
 		}
 		
+		acc6.withdraw(200.0);
+		System.out.println(acc6.getBalance());
+		
+		sacc7.withdraw(200.0);
+		System.out.println(sacc7.getBalance());
+		
+		bacc8.withdraw(200.0);
+		System.out.println(bacc8.getBalance());
 	}
+	
 }
--- bank/src/entities/BusinessAccount.java
+++ bank/src/entities/BusinessAccount.java
@@ -24,6 +24,12 @@ public class BusinessAccount extends Account{
 	public void loan(double amount) {
 		if(amount <= loanLimit) {
 		balance += amount - 10.0;
-		}
+		}		
+	}
+	
+	@Override
+	public final void withdraw(double amount) {
+		super.withdraw(amount);
+		balance -= 2.0;
 	}
 }
