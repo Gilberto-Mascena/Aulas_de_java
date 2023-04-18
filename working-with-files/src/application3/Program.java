c5b0f41de492c15e5f192f723440fc803beeb23d
Gilberto Mascena <gilbertomascena@gmail.com>
2023-04-17 21:46:53 -0300

    Add new files

---
+++ working-with-files/src/application3/Program.java
@@ -0,0 +1,25 @@
+package application3;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		String[] lines = new String[] {"José", "Maria", "Antônio", "Alice"};
+		
+		String path = "/Users/gilbertosilva/Desktop/Terminal/Praticando_no_terminal1.txt";
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path/*, true*/))) {
+			
+			for(String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		}
+		catch (IOException e) {
+				e.getMessage();				}
+	}
+}
